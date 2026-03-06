6d75db206ecdfcb6bb39c0fd1c72ef84a1ed9af2
samisfam <sf3318@columbia.edu>
2026-03-06 10:44:22 -0500

    ruff format

--- eviction.py
+++ eviction.py
@@ -5,6 +5,7 @@ import requests
 
 # https://data.cityofnewyork.us/City-Government/Evictions/6z8x-wfk4/about_data
 
+
 def eviction():
     eviction_data = pd.DataFrame()
     url = "https://data.cityofnewyork.us/resource/6z8x-wfk4.json"
@@ -13,21 +14,22 @@ def eviction():
     loop = True
     while loop:
         params = {"$limit": limit, "$offset": offset}
-        r = requests.get(url, params = params)
-        data =r.json()
+        r = requests.get(url, params=params)
+        data = r.json()
         clean_pop = pd.json_normalize(data)
-        eviction_data = pd.concat([eviction_data,clean_pop])
+        eviction_data = pd.concat([eviction_data, clean_pop])
 
         if len(data) < limit:
             loop = False
 
         offset += limit
-    eviction_data_clean = eviction_data[["executed_date","borough"]]
+    eviction_data_clean = eviction_data[["executed_date", "borough"]]
     eviction_data_clean["executed_date"] = pd.to_datetime(eviction_data_clean["executed_date"])
 
     return eviction_data_clean
 
+
 def borough_count(eviction_data_clean):
-    eviction_data_group = eviction_data_clean.groupby("borough").size().reset_index(name = "Count")
+    eviction_data_group = eviction_data_clean.groupby("borough").size().reset_index(name="Count")
 
     return eviction_data_group
--- test_utils.py
+++ test_utils.py
@@ -5,35 +5,36 @@ from utils import borough_count, health_year_filter
 
 
 def test_health_year_filter():
-    df = pd.DataFrame({
-        "year": ["2020", "2020", "2019"],
-        "Prevelance": ["Prevalence", "Prevalence", "Prevalence"],
-        "No Health Insurance": [12.6, 6.6, 12.7]
-    })
+    df = pd.DataFrame(
+        {
+            "year": ["2020", "2020", "2019"],
+            "Prevelance": ["Prevalence", "Prevalence", "Prevalence"],
+            "No Health Insurance": [12.6, 6.6, 12.7],
+        }
+    )
 
     result = health_year_filter(df, 2020, "No Health Insurance")
     health_expected_value = 12.6
     assert result == health_expected_value
 
+
 def test_health_year_filter_not_found():
-    df = pd.DataFrame({
-        "year": ["2020", "2020", "2019"],
-        "Prevelance": ["Prevalence", "Prevalence", "Prevalence"],
-        "No Health Insurance": [12.6, 6.6, 12.7]
-    })
+    df = pd.DataFrame(
+        {
+            "year": ["2020", "2020", "2019"],
+            "Prevelance": ["Prevalence", "Prevalence", "Prevalence"],
+            "No Health Insurance": [12.6, 6.6, 12.7],
+        }
+    )
 
     with pytest.raises(KeyError):
         health_year_filter(df, 2020, "Column does not exist")
 
+
 def test_borough_count():
-    test_df = pd.DataFrame({
-        "borough": ["Manhattan",
-                    "Brooklyn",
-                    "Brooklyn",
-                    "Queens",
-                    "Manhattan"
-                    ]
-    })
+    test_df = pd.DataFrame(
+        {"borough": ["Manhattan", "Brooklyn", "Brooklyn", "Queens", "Manhattan"]}
+    )
 
     result = borough_count(test_df)
     borough_expected_value = 2
@@ -42,16 +43,10 @@ def test_borough_count():
     assert brooklyn_count == borough_expected_value
     assert manhattan_count == borough_expected_value
 
+
 def test_borough_count_empty():
-    df = pd.DataFrame({
-        "borough": []
-    })
+    df = pd.DataFrame({"borough": []})
 
     result = borough_count(df)
 
     assert result.empty
-
-
-
-
-
