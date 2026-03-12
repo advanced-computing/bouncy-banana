941810c0c6263dbd349c8c18a6450f846f3c9012
Sam <sf3318@columbia.edu>
2026-03-11 22:39:19 -0400

    evictions data fix

--- page_2.py
+++ page_2.py
@@ -12,7 +12,7 @@ nyc_map = folium.Map(location=[40.7128, -74.0060], zoom_start=11)
 
 for _i, r in eviction_data.iterrows():
     folium.Marker(
-        location=[r["latitude"], r["longitude"]], popup=r["address"], tooltip="Eviction"
+        location=[r["latitude"], r["longitude"]], popup=r["eviction_address"], tooltip="Eviction"
     ).add_to(nyc_map)
 
 st.title("NYC Eviction Data")
