786e0dbe5526c9e622dd68c25e782f6aca7304aa
Sam <sf3318@columbia.edu>
2026-03-11 22:21:06 -0400

    eviction data fix

--- eviction.py
+++ eviction.py
@@ -24,13 +24,13 @@ def eviction():
         #     loop = False
         offset += limit
         count += 1
-    eviction_data_clean = eviction_data[["executed_date", "borough"]]
+    eviction_data_clean = eviction_data[["executed_date", "borough","longitude","latitude"]]
     eviction_data_clean["executed_date"] = pd.to_datetime(eviction_data_clean["executed_date"])
     eviction_data_clean["longitude"] = pd.to_numeric(
         eviction_data_clean["longitude"], errors="coerce"
     )
     eviction_data_clean["latitude"] = pd.to_numeric(
-        eviction_data_clean["latitiud"], errors="coerce"
+        eviction_data_clean["latitude"], errors="coerce"
     )
     return eviction_data_clean
 
